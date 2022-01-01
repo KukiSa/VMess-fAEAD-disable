d46172f3a5cff663246d5464a041660478dce340
Signaliks <youngdo@jiangxia.gov.cn>
2022-01-01 14:26:41 +0800

    Delete VMess-fAEAD-disable-nostatus.sh

--- VMess-fAEAD-disable-nostatus.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-serFile=$1
-[ -z "$serFile" ] && serFile="/etc/systemd/system/v2ray.service"
-sed -i '/ExecStart/i\Environment="V2RAY_VMESS_AEAD_FORCED=false"' $serFile
-systemctl daemon-reload
-systemctl restart v2ray
-exit
