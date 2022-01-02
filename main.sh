3ee09320a42d4a207570a3da9a49c14d5b8b8ba0
wulabing <wulabing@gmail.com>
2022-01-02 12:32:54 +0800

    Add Environment repeatedly fix

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 
 read -rp "Please enter the path of v2ray.service (default: /etc/systemd/system/v2ray.service)：" serFile
 [ -z "$serFile" ] && serFile="/etc/systemd/system/v2ray.service"
+sed -i '/Environment="V2RAY_VMESS_AEAD_FORCED=false"/d' $serFile
 sed -i '/ExecStart/i\Environment="V2RAY_VMESS_AEAD_FORCED=false"' $serFile
 systemctl daemon-reload
 systemctl restart v2ray
